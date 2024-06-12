832507d13e151fee60ee706022e29273bf300b70
Jennifer Rahman <81711306+jb520@users.noreply.github.com>
2024-06-12 17:14:57 +0000

    create director paths

---
+++ app/controllers/director_controller.rb
@@ -0,0 +1,12 @@
+class DirectorController < ApplicationController
+  def list_directors
+    render ({ :template => "msm_templates/directors_list"})
+  end
+
+  def youngest_director
+    render ({ :template => "msm_templates/youngest_director" })
+  end
+
+  def eldest_director
+    render ({ :template => "msm_templates/eldest_director" })
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
+  get("/directors", { :controller => "director", :action => "list_directors" })
+  get("/directors/youngest", { :controller => "", :action => "" })
+  get("/directors/eldest", { :controller => "", :action => "" })
+  get("/movies", { :controller => "", :action => "" })
+  get("/actors", { :controller => "", :action => "" })
 end
