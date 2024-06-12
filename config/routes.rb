6bd526be43f704577ccf092238632e921197a2d1
Jennifer Rahman <81711306+jb520@users.noreply.github.com>
2024-06-12 17:16:33 +0000

    fix director routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
   get("/directors", { :controller => "director", :action => "list_directors" })
-  get("/directors/youngest", { :controller => "", :action => "" })
-  get("/directors/eldest", { :controller => "", :action => "" })
+  get("/directors/youngest", { :controller => "director", :action => "youngest_director" })
+  get("/directors/eldest", { :controller => "director", :action => "eldest_director" })
   get("/movies", { :controller => "", :action => "" })
   get("/actors", { :controller => "", :action => "" })
 end
