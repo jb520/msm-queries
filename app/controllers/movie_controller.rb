b47d534155a9b0aa2f80ec8d4702fd28fc4ea2d7
Jennifer Rahman <81711306+jb520@users.noreply.github.com>
2024-06-13 17:28:33 +0000

    update movie details page

--- app/controllers/movie_controller.rb
+++ app/controllers/movie_controller.rb
@@ -2,5 +2,11 @@ class MovieController < ApplicationController
   def list_movies
     render ({ :template => "msm_templates/movies_list"})
   end
-end
 
+  def movie_details
+    the_id = params.fetch("an_id")
+    matching_records = Movie.where({ :id => the_id })
+    @the_movie = matching_records.at(0)
+    render ({ :template => "msm_templates/movies_details"})
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get("/directors/eldest", { :controller => "director", :action => "eldest_director" })
   get("/directors/:an_id", { :controller => "director", :action => "show_details" })
   get("/movies", { :controller => "movie", :action => "list_movies" })
+  get("/movies/:an_id", { :controller => "movie", :action => "movie_details" })
   get("/actors", { :controller => "actor", :action => "list_actors" })
 end
