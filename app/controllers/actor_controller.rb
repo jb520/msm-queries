0c5e2898abbc084da72dbc2eb8f70f599b90f78a
Jennifer Rahman <81711306+jb520@users.noreply.github.com>
2024-06-14 19:15:00 +0000

    debug actor details page

--- app/controllers/actor_controller.rb
+++ app/controllers/actor_controller.rb
@@ -7,6 +7,6 @@ class ActorController < ApplicationController
     the_id = params.fetch("an_id")
     matching_records = Actor.where({ :id => the_id })
     @the_actor = matching_records.at(0)
-    render ({ :template => "msm_templates/actors_details"})
+    render ({ :template => "msm_templates/actors_details" })
   end
 end
